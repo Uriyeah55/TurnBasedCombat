1472f8150b59542630c79ec9d61dba14723165d4
Oriol <74713703+Uriyeah55@users.noreply.github.com>
2024-11-04 01:45:50 +0100

    cant repeat skill in shuffle mode

--- Assets/SkillCardUI.cs
+++ Assets/SkillCardUI.cs
@@ -1,63 +1,63 @@
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
-    using UnityEngine.UI;
-    using UnityEngine.EventSystems;
-    using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
 
+public class SkillCardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public TMP_Text skillNameText, skillDescription;
+    public Image image;
 
-    public class SkillCardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    private Vector3 originalPosition;
+    private Vector3 hoverOffset = new Vector3(0, 20, 0);
+    private RectTransform cardRectTransform;  
+    public GameObject manager, shuffleMenu;
+    UIBattleManager uiBattleManager;
+
+    public Skill currentSkill;
+
+    // Lista de los slots de shuffle
+    public List<Image> shuffleSlots = new List<Image>();
+
+    private bool isHovered = false;
+
+    void Start()
     {
-        public TMP_Text skillNameText, skillDescription;
-    // public Sprite skillIcon;
-        public Image image;
+        manager = GameObject.Find("MANAGER");
+        uiBattleManager=manager.GetComponent<UIBattleManager>();
+        hideInfoTexts();
 
-        private Vector3 originalPosition;
-        private Vector3 hoverOffset = new Vector3(0, 20, 0);
-        private RectTransform cardRectTransform;  
-        public GameObject manager, shuffleMenu;
+        // Obtener el RectTransform de la propia carta
+        cardRectTransform = GetComponent<RectTransform>();
+        originalPosition = cardRectTransform.anchoredPosition;
 
-        public Skill currentSkill;
+        // Obtener todos los componentes Image en los hijos de ShuffleMenu
+        shuffleSlots.AddRange(shuffleMenu.GetComponentsInChildren<Image>());
 
-        // Lista de los slots de shuffle
-        public List<Image> shuffleSlots = new List<Image>();
-
-        private bool isHovered = false;
-
-        void Start()
+        if (shuffleSlots.Count == 0)
         {
-            manager = GameObject.Find("MANAGER");
-           // shuffleMenu = GameObject.Find("ShuffleMenu");
-            hideInfoTexts();
+            Debug.LogError("No shuffle slots found in ShuffleMenu.");
+        }
+    }
 
-            // Obtener el RectTransform de la propia carta
-            cardRectTransform = GetComponent<RectTransform>();
-            originalPosition = cardRectTransform.anchoredPosition;
-
-            // Obtener todos los componentes Image en los hijos de ShuffleMenu
-            shuffleSlots.AddRange(shuffleMenu.GetComponentsInChildren<Image>());
-
-            if (shuffleSlots.Count == 0)
-            {
-                Debug.LogError("No shuffle slots found in ShuffleMenu.");
-            }
+    public void SetSkillData(Skill skill)
+    {
+        if (skill == null)
+        {
+            Debug.LogError("Skill passed to SetSkillData is null.");
+            return;
         }
 
-        public void SetSkillData(Skill skill)
-        {
-            if (skill == null)
-            {
-                Debug.LogError("Skill passed to SetSkillData is null.");
-                return;
-            }
+        Debug.Log("Setting skill data for: " + skill.skillName);
+        skillNameText.text = skill.skillName;
+        skillDescription.text = skill.skillDescription;
+        image.sprite = skill.skillSprite; // Update sprite directly
+        currentSkill = skill;
+    }
 
-            Debug.Log("Setting skill data for: " + skill.skillName);
-            skillNameText.text = skill.skillName;
-            skillDescription.text = skill.skillDescription;
-    image.sprite = skill.skillSprite;
-            currentSkill = skill;
-        }
-        public void OnPointerEnter(PointerEventData eventData)
+    public void OnPointerEnter(PointerEventData eventData)
     {
         if (!isHovered)
         {
@@ -66,6 +66,7 @@
             showInfoTexts();
         }
     }
+
     public void OnPointerExit(PointerEventData eventData)
     {
         if (isHovered)
@@ -76,32 +77,39 @@
         }
     }
 
-    public void OnPointerClick(PointerEventData eventData)
+public void OnPointerClick(PointerEventData eventData)
+{
+    // Verificar el valor de isShuffling
+    if (GlobalVars.isShuffling)
     {
-       
-            // Verificar el valor de isShuffling
-            if (GlobalVars.isShuffling)
+        // Lógica actual de shuffle
+        ShuffleSlot emptySlot = GetEmptyShuffleSlot();
+        if (emptySlot != null)
+        {
+            // Check if the skill has already been used
+            if (!uiBattleManager.usedSkills.Contains(currentSkill))
             {
-                // Lógica actual de shuffle
-                ShuffleSlot emptySlot = GetEmptyShuffleSlot();
-                if (emptySlot != null)
-                {
-                    emptySlot.SetSkill(currentSkill);
-                    emptySlot.gameObject.GetComponent<Image>().sprite = image.sprite;
-
-                    Debug.Log("Skill placed in shuffle slot: " + currentSkill.skillName);
-                }
-                else
-                {
-                    Debug.Log("All shuffle slots are full.");
-                }
+                emptySlot.SetSkill(currentSkill);
+                emptySlot.gameObject.GetComponent<Image>().sprite = image.sprite;
+                uiBattleManager.AddSkillToUsed(currentSkill);
+                Debug.Log("Skill placed in shuffle slot: " + currentSkill.skillName);
             }
             else
             {
-                // Mostrar información de la skill
-                Debug.Log($"Skill: {currentSkill.skillName}, Description: {currentSkill.skillDescription}");
+                Debug.Log("Skill has already been used.");
             }
+        }
+        else
+        {
+            Debug.Log("All shuffle slots are full.");
+        }
     }
+    else
+    {
+        // Mostrar información de la skill
+        Debug.Log($"Skill: {currentSkill.skillName}, Description: {currentSkill.skillDescription}");
+    }
+}
 
 
     private ShuffleSlot GetEmptyShuffleSlot()
@@ -122,19 +130,15 @@
         return null;  // No hay slots vacíos
     }
 
-
-
-
-
-        public void showInfoTexts()
-        {
-            skillNameText.enabled = true;
-            skillDescription.enabled = true;
-        }
-
-        public void hideInfoTexts()
-        {
-            skillNameText.enabled = false;
-            skillDescription.enabled = false;
-        }
+    public void showInfoTexts()
+    {
+        skillNameText.enabled = true;
+        skillDescription.enabled = true;
     }
+
+    public void hideInfoTexts()
+    {
+        skillNameText.enabled = false;
+        skillDescription.enabled = false;
+    }
+}
--- Assets/UIBattleManager.cs
+++ Assets/UIBattleManager.cs
@@ -3,26 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class UIBattleManager : MonoBehaviour
 {
-
-    public Button btnSkills,btnShuffle,btnBack,btnGenre,btnClear;
-    public GameObject skillCardPanel,canvasCombat,camBehindPlayer,shuffleMenu;
+    public Button btnSkills, btnShuffle, btnBack, btnGenre, btnClear;
+    public GameObject skillCardPanel, canvasCombat, camBehindPlayer, shuffleMenu;
     public GameObject[] shuffleBack;
     public GameObject shuffleBackGlobal;
 
     public AnimationManager animManager;
 
-     public List<ShuffleSlot> shuffleSlots = new List<ShuffleSlot>(); // Lista de todos los slots de shuffle
+    public List<ShuffleSlot> shuffleSlots = new List<ShuffleSlot>(); // Lista de todos los slots de shuffle
+    public List<Skill> usedSkills = new List<Skill>(); // Nueva lista para habilidades usadas
     Animator playerAC;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Obtain player Animator from the Animation Manager script
-        animManager=GetComponent<AnimationManager>();
-        playerAC=animManager.playerAnimCtrler;
+        // Obtener el Animator del jugador desde el script AnimationManager
+        animManager = GetComponent<AnimationManager>();
+        playerAC = animManager.playerAnimCtrler;
     }
 
     // Update is called once per frame
@@ -30,10 +29,11 @@ public class UIBattleManager : MonoBehaviour
     {
         
     }
-     public void  showSkills()
-     {
 
-        if(GlobalVars.isShuffling){
+    public void showSkills()
+    {
+        if (GlobalVars.isShuffling)
+        {
             shuffleBackGlobal.SetActive(true);
             shuffleMenu.SetActive(true);
             btnClear.gameObject.SetActive(true);
@@ -43,7 +43,8 @@ public class UIBattleManager : MonoBehaviour
                 backSlot.SetActive(true);
             }
         }
-        else{
+        else
+        {
             shuffleMenu.SetActive(false);
             btnClear.gameObject.SetActive(false);
 
@@ -52,62 +53,74 @@ public class UIBattleManager : MonoBehaviour
                 backSlot.SetActive(false);
                 backSlot.gameObject.SetActive(false);
             }
-
         }
         btnSkills.gameObject.SetActive(false);
         btnShuffle.gameObject.SetActive(false);
         btnGenre.gameObject.SetActive(false);
 
         playerAC.SetInteger("currentStance", 2);
-		skillCardPanel.SetActive(true);
-		canvasCombat.GetComponent<HorizontalSpacingLerp>().StartLerpingSpacing(-40f, -100f, .2f);
+        skillCardPanel.SetActive(true);
+        canvasCombat.GetComponent<HorizontalSpacingLerp>().StartLerpingSpacing(-40f, -100f, .2f);
         btnBack.gameObject.SetActive(true);
-		//CameraTransitionManager.GetComponent<CameraTransitionManager>().StartCameraTransition(1,2);
-		camBehindPlayer.GetComponent<Animator>().SetInteger("currentState",0);
+        camBehindPlayer.GetComponent<Animator>().SetInteger("currentState", 0);
     }
-         public void  showShuffleMenu()
-     {
+
+    public void showShuffleMenu()
+    {
         btnSkills.gameObject.SetActive(false);
         playerAC.SetInteger("currentStance", 2);
-		skillCardPanel.SetActive(true);
-		canvasCombat.GetComponent<HorizontalSpacingLerp>().StartLerpingSpacing(-40f, -100f, .2f);
+        skillCardPanel.SetActive(true);
+        canvasCombat.GetComponent<HorizontalSpacingLerp>().StartLerpingSpacing(-40f, -100f, .2f);
         btnBack.gameObject.SetActive(true);
-		//CameraTransitionManager.GetComponent<CameraTransitionManager>().StartCameraTransition(1,2);
-		camBehindPlayer.GetComponent<Animator>().SetInteger("currentState",0);
+        camBehindPlayer.GetComponent<Animator>().SetInteger("currentState", 0);
     }
-        public void showSkillButtons()
-        {
-		camBehindPlayer.GetComponent<Animator>().SetInteger("currentState",1);
+
+    public void showSkillButtons()
+    {
+        camBehindPlayer.GetComponent<Animator>().SetInteger("currentState", 1);
 
         btnSkills.gameObject.SetActive(true);
         btnShuffle.gameObject.SetActive(true);
         btnGenre.gameObject.SetActive(true);
-		skillCardPanel.SetActive(false);
+        skillCardPanel.SetActive(false);
         btnClear.gameObject.SetActive(false);
-
-
     }
+
+    public void ClearSelectedSkills()
+    {
+        // Limpia todos los slots de shuffle
+        foreach (ShuffleSlot slot in shuffleMenu.GetComponentsInChildren<ShuffleSlot>())
+        {
+            slot.ClearSlot();
+        }
+
+        // Limpia la lista de habilidades usadas
+        usedSkills.Clear();
+
+        Debug.Log("All shuffle slots and used skills list have been cleared.");
+    }
+
     public void hideShuffleMenu()
     {
         shuffleMenu.SetActive(false);
         btnClear.gameObject.SetActive(false);
         foreach (GameObject backSlot in shuffleBack)
-            {
-                backSlot.SetActive(false);
-                backSlot.gameObject.SetActive(false);
-            }
+        {
+            backSlot.SetActive(false);
+            backSlot.gameObject.SetActive(false);
+        }
     }
-    public void hideSkillButtons(){
 
+    public void hideSkillButtons()
+    {
         btnSkills.gameObject.SetActive(false);
         btnShuffle.gameObject.SetActive(false);
         btnGenre.gameObject.SetActive(false);
-		skillCardPanel.SetActive(false);
+        skillCardPanel.SetActive(false);
         btnBack.gameObject.SetActive(false);
-
     }
 
-        public void ClearAllShuffleSlots()
+    public void ClearAllShuffleSlots()
     {
         showShuffleBacks();
         foreach (ShuffleSlot slot in shuffleSlots)
@@ -120,28 +133,40 @@ public class UIBattleManager : MonoBehaviour
         Debug.Log("All shuffle slots have been cleared.");
     }
 
-    public void showShuffleBacks(){
-            foreach (GameObject backSlot in shuffleBack)
-            {
-                backSlot.SetActive(true);
-                //backSlot.gameObject.SetActive(true);
-            }
+    public void showShuffleBacks()
+    {
+        foreach (GameObject backSlot in shuffleBack)
+        {
+            backSlot.SetActive(true);
+        }
     }
-    	static public void activateShuffleMode()
-        {
-		    GlobalVars.isShuffling=true;
-	    }
-        static public void disableShuffleMode()
-        {
-		    GlobalVars.isShuffling=false;
-	    }
-		void hideAudioButtons(){
 
-        }
-		void hideAttackName(){
+    static public void activateShuffleMode()
+    {
+        GlobalVars.isShuffling = true;
+    }
 
+    static public void disableShuffleMode()
+    {
+        GlobalVars.isShuffling = false;
+    }
+
+    public void AddSkillToUsed(Skill skill)
+    {
+        if (!usedSkills.Contains(skill))
+        {
+            usedSkills.Add(skill);
+            Debug.Log($"Skill {skill.skillName} added to used skills.");
         }
-        void showAttackName(){
-            
+        else
+        {
+            Debug.Log($"Skill {skill.skillName} is already used.");
         }
+    }
+
+    public void ClearUsedSkills()
+    {
+        usedSkills.Clear();
+        Debug.Log("Used skills list has been cleared.");
+    }
 }
